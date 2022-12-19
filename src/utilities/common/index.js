152ecbefd96779e2d860f7d529769261126a157d
Reshin P <reshin.pulapaddy@cesltd.com>
2022-12-19 14:45:58 +0530

    added dynamic filter function

--- src/utilities/common/index.js
+++ src/utilities/common/index.js
@@ -1,15 +1,26 @@
 export const setItem = (key, value) => {
   try {
-    localStorage.removeItem(key)
+    localStorage.removeItem(key);
     if (value) {
-      localStorage.setItem(key, value)
+      localStorage.setItem(key, value);
     }
   } catch (error) {
     // console.log('>>>>: src/helpers/Utils.js : setCurrentUser -> error', error)
   }
-}
+};
 
 export const removeItem = (key) => {
-  localStorage.removeItem(key)
-}
-export const getItem = (key) => localStorage.getItem(key)
+  localStorage.removeItem(key);
+};
+export const getItem = (key) => localStorage.getItem(key);
+
+export const handleFilterChange = (e, data, label, setResult) => {
+  if (e.target.checked === false) {
+    setResult(data);
+  } else {
+    const filterData = data.filter((item) => {
+      if (item[e.target.name] === label) return item;
+    });
+    setResult(filterData);
+  }
+};
