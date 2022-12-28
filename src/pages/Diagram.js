398575687711439fe01519d9a1880b12507711b6
naresh-FD <116813860+Naresh-CES@users.noreply.github.com>
2022-12-28 15:05:18 +0530

    added sidebar in drawing toll page

--- src/pages/Diagram.js
+++ src/pages/Diagram.js
@@ -25,6 +25,7 @@ import { sampleGraphs } from './Diagram/config/sample-graphs';
 
 import './Diagram/css/style.css';
 import './Diagram/css/theme-picker.css';
+import '../assets/scss/DrawToolPage/DrawToolPage.scss';
 
 class Diagram extends Component {
   constructor(props) {
@@ -68,15 +69,60 @@ class Diagram extends Component {
           <div className='app-title'>
             <img src='/logo512.png' className='p-2 img-fluid' alt='' />
           </div>
-          <div className='toolbar-container' />
         </div>
         <div className='app-body'>
           <div className='stencil-container' />
           <div className='paper-container' />
-          <div className='inspector-container' />
+          <div className='sidebar'>
+            <ul className='nav nav-tabs custom-tabs' id='myTab' role='tablist'>
+              <li className='nav-item' role='presentation'>
+                <button
+                  className='nav-link active'
+                  id='home-tab'
+                  data-bs-toggle='tab'
+                  data-bs-target='#home'
+                  type='button'
+                  role='tab'
+                  aria-controls='home'
+                  aria-selected='true'
+                >
+                  Home
+                </button>
+              </li>
+              <li className='nav-item' role='presentation'>
+                <button
+                  className='nav-link'
+                  id='profile-tab'
+                  data-bs-toggle='tab'
+                  data-bs-target='#profile'
+                  type='button'
+                  role='tab'
+                  aria-controls='profile'
+                  aria-selected='false'
+                >
+                  Profile
+                </button>
+              </li>
+            </ul>
+          </div>
+
           <div className='navigator-container' />
         </div>
         <div ref={this.themePickerRef} className='theme-picker'></div>
+
+        <div class='tab-content  inspector-container' id='myTabContent'>
+          <div
+            class='tab-pane fade show active'
+            id='home'
+            role='tabpanel'
+            aria-labelledby='home-tab'
+          >
+            <div className='inspector-container' style={{ border: '10px solid red' }} />
+          </div>
+          <div class='tab-pane fade' id='profile' role='tabpanel' aria-labelledby='profile-tab'>
+            <div className='toolbar-container' />
+          </div>
+        </div>
       </div>
     );
   }
