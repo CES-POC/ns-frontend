b2c2f1a9ae479c0eec36c99c7e9908b25557f2e5
Rajkumar1510 <srajkumars1510@gmail.com>
2023-01-03 17:21:14 +0530

    formbuilder page sidebar works

--- src/components/sideBar/SideBar.js
+++ src/components/sideBar/SideBar.js
@@ -1,28 +1,39 @@
-import React, { useState } from 'react';
-import { BsPlusCircleDotted } from 'react-icons/bs';
-import { Link } from 'react-router-dom';
-import '../../assets/scss/sideBar/sideBar.scss';
-import Modal from '../Modal/Modal';
+import React, { useEffect, useState } from "react";
+import { BsPlusCircleDotted } from "react-icons/bs";
+import { Link, useLocation } from "react-router-dom";
+import "../../assets/scss/sideBar/sideBar.scss";
+import { Constants } from "../../utilities/constants";
+import Modal from "../Modal/Modal";
 
 const SideBar = () => {
   const [modalOpen, setModalOpen] = useState(false);
-
+  const [sideBarClass, setSideBarClass] = useState("sideBar-mainDiv");
+  const { pathname } = useLocation();
+  
+  useEffect(() => {
+    if (pathname === '/formbuilder') {
+      setSideBarClass("d-none");
+    }
+  }, []);
   const handleOpenModal = () => {
     setModalOpen(true);
   };
   return (
-    <div className='sideBar-mainDiv'>
+    <div className={sideBarClass}>
       <div>
         <h5 className='sideNav'>
           Create System
           <span className='px-2'>
-            <BsPlusCircleDotted data-bs-toggle='modal' data-bs-target='#staticBackdrop' />
+            <BsPlusCircleDotted
+              data-bs-toggle='modal'
+              data-bs-target='#staticBackdrop'
+            />
           </span>
         </h5>
         <h5 className='sideNav'>
           Create Data Form
           <span className='px-2'>
-            <Link className='text-light' to={'/formbuilder'}>
+            <Link className='text-light' to={"/formbuilder"}>
               <BsPlusCircleDotted />
             </Link>
           </span>
